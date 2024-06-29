e9f11951c081df55d2eb48f5ccdc2fd6a48076e0
extclp <2842417407@qq.com>
2024-06-29 20:19:20 +0800

    update

--- src/pages/teacher/index.vue
+++ src/pages/teacher/index.vue
@@ -1,41 +1,55 @@
 <template>
-  <scroll-view>
-    <sar-card class="m4">
-      <view class="flex gap-2">
-        <view class="text-center">
-          <nut-avatar class="mb-2" :size="70" />
-          <view>最近登录</view>
-          <view>今天</view>
-        </view>
-        <view class="flex-1">
-          <text class="float-right">
-            <text class="text-primary">¥110</text>
-            <text class="text-[#AAA6B9]">/小时</text>
-          </text>
-          <text>朱飒</text>
-          <nut-tag type="primary" class="ml2">研究生教员</nut-tag>
-          <view class="mt-6">
-            <text class="w-8 text-primary">科目：</text>
-            <text>初中数学</text>
+  <view class="flex h-full flex-col">
+    <view>
+      <sar-dropdown>
+        <sar-dropdown-item label="教龄" :options="makeLV(dataList?.tutor_age)" />
+        <sar-dropdown-item label="区域" :options="makeLV(dataList?.teachingPosition)" />
+        <sar-dropdown-item v-model="form.teaching_position" label="价格" :options="makeLV(dataList?.cost)" />
+      </sar-dropdown>
+    </view>
+    <scroll-view scroll-y @scrolltolower="loadMore" class="flex-1 overflow-hidden">
+      <sar-card class="m4" v-for="item in data">
+        <view class="flex gap-2">
+          <view class="text-center">
+            <nut-avatar class="mb-2" :size="70" />
+            <view>最近登录</view>
+            <view>{{ item.login_time }}</view>
           </view>
-          <view class="mt-2">
-            <text class="w-8 text-primary">地区：</text>
-            <text>崇川区</text>
+          <view class="flex-1">
+            <text class="float-right">
+              <text class="text-primary">¥110</text>
+              <text class="text-[#AAA6B9]">/小时</text>
+            </text>
+            <text>朱飒</text>
+            <nut-tag type="primary" class="ml2">研究生教员</nut-tag>
+            <view class="mt-6">
+              <text class="w-8 text-primary">科目：</text>
+              <text>初中数学</text>
+            </view>
+            <view class="mt-2">
+              <text class="w-8 text-primary">地区：</text>
+              <text>崇川区</text>
+            </view>
           </view>
         </view>
-      </view>
-      <view class="mt-2 flex gap-2">
-        <nut-tag type="primary">重庆大学</nut-tag>
-        <nut-tag type="primary">数学师范 研究生</nut-tag>
-      </view>
-    </sar-card>
-    <nut-infiniteloading />
-  </scroll-view>
+        <view class="mt-2 flex gap-2">
+          <nut-tag type="primary">{{ item.school }}</nut-tag>
+          <nut-tag type="primary">数学师范 研究生</nut-tag>
+        </view>
+      </sar-card>
+      <sar-load-more :status="loadMoreStautus" />
+    </scroll-view>
+  </view>
 </template>
 
 <script setup lang="ts">
 import { teachers } from '@/api'
 import type { Teacher } from '@/api/interfaces';
+import { listData } from '@/store';
+import { makeLV } from '@/utls';
+import type { LoadMoreStatus } from 'sard-uniapp';
+
+const dataList = listData();
 
 const form = reactive({
   page: 1,
@@ -47,11 +61,30 @@ const form = reactive({
   teaching_position: '幸福附近',
 })
 
-const data = ref<Teacher[]>();
+const data = ref<Teacher[]>([]);
 
-teachers(form).then((resp) => {
-  data.value = resp.data.teachers
-})
+const loadMoreStautus = ref<LoadMoreStatus>("incomplete")
+
+function getList() {
+  loadMoreStautus.value = 'loading'
+  teachers(form).then((resp) => {
+    const list = resp.data.teachers;
+    data.value.push(...list)
+    if (list.length == 0) {
+      loadMoreStautus.value = 'complete'
+    } else {
+      loadMoreStautus.value = 'incomplete'
+    }
+  })
+}
+getList()
+
+function loadMore() {
+  if (loadMoreStautus.value != 'complete') {
+    form.page++
+    getList();
+  }
+}
 </script>
 
 <style></style>
--- src/utls/index.ts
+++ src/utls/index.ts
@@ -1,10 +1,21 @@
 import dayjs from 'dayjs'
 import 'dayjs/locale/zh-cn.js'
 import relativeTime from 'dayjs/plugin/relativeTime'
+import { isDef } from 'nutui-uniapp/components/_utils/is.js'
 
 dayjs.locale('zh-cn')
 dayjs.extend(relativeTime)
 
 export function ago(str: string) {
   return dayjs(str).fromNow()
+}
+
+export function makeLV(list?: string[]) {
+  if (isDef(list))
+    return list.map(it => ({ label: it, value: it }))
+}
+
+export function makeTV(list?: string[]) {
+  if (isDef(list))
+    return list.map(it => ({ text: it, value: it }))
 }
